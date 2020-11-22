28fc8a820a895303435e6b835afedc8a025487a8
Juan Avila <dev@juanavila.es>
2020-11-22 14:19:32 +0100

    [Passive view] Remove Logic lazy class

--- src/main/java/usantatecla/draughts/Draughts.java
+++ src/main/java/usantatecla/draughts/Draughts.java
@@ -1,23 +1,40 @@
 package usantatecla.draughts;
 
-import usantatecla.draughts.controllers.Logic;
-import usantatecla.draughts.controllers.InteractorController;
+import usantatecla.draughts.controllers.*;
+import usantatecla.draughts.models.Game;
+import usantatecla.draughts.models.State;
+import usantatecla.draughts.models.StateValue;
 import usantatecla.draughts.views.View;
+
+import java.util.HashMap;
+import java.util.Map;
 
 class Draughts {
     
-    private View view;
     private Logic logic;
+    private Game game;
+    private State state;
+    private Map<StateValue, InteractorController> controllers;
 
     private Draughts(){
-        this.view = new View();
         this.logic = new Logic();
+        this.game = new Game();
+        this.state = new State();
+        this.controllers = new HashMap<StateValue, InteractorController>();
+        this.controllers.put(StateValue.INITIAL, new StartController(this.game, this.state));
+        this.controllers.put(StateValue.IN_GAME, new PlayController(this.game, this.state));
+        this.controllers.put(StateValue.FINAL, new ResumeController(this.game, this.state));
+        this.controllers.put(StateValue.EXIT, null);
+    }
+
+    public InteractorController getController() {
+        return this.controllers.get(this.state.getValueState());
     }
 
     private void play() {
         InteractorController controller;
 		do {
-			controller = this.logic.getController();
+			controller = this.getController();
 			if (controller != null)
 				controller.control();
 		} while (controller != null); 
