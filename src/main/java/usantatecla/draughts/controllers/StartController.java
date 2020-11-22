bace0861239a3d3f2fa1051001de09e2f940f3ba
Juan Avila <dev@juanavila.es>
2020-11-22 13:11:26 +0100

    [Passive view] Pull up common methods

--- src/main/java/usantatecla/draughts/controllers/StartController.java
+++ src/main/java/usantatecla/draughts/controllers/StartController.java
@@ -1,22 +1,13 @@
 package usantatecla.draughts.controllers;
 
-import usantatecla.draughts.models.Coordinate;
 import usantatecla.draughts.models.Game;
-import usantatecla.draughts.models.Piece;
 import usantatecla.draughts.models.State;
 import usantatecla.draughts.views.View;
 
 public class StartController extends InteractorController {
 
-	private View view;
-
 	public StartController(Game game, State state) {
         super(game, state);
-        this.view = new View();
-	}
-
-	View getView() {
-		return view;
 	}
 
 	public void start() {
@@ -33,24 +24,5 @@ public class StartController extends InteractorController {
 		this.getView().writeln(View.TITTLE);
 		this.write();
 		this.start();
-	}
-
-	void write() {
-		this.getView().writeNumbersLine(this.getDimension());
-		for (int i = 0; i < this.getDimension(); i++)
-			this.writePiecesRow(i);
-		this.getView().writeNumbersLine(this.getDimension());
-	}
-
-	void writePiecesRow(final int row) {
-		this.getView().write((row + 1) + "");
-		for (int j = 0; j < this.getDimension(); j++) {
-			Piece piece = this.getPiece(new Coordinate(row, j));
-			if (piece == null)
-				this.getView().write(" ");
-			else
-				this.getView().write(piece.getCode());
-		}
-		this.getView().writeln((row + 1) + "");
 	}
 }
