3c80053866ca66cf85b6f93ffb4b476856573cea
Juan Avila <dev@juanavila.es>
2020-11-22 14:41:44 +0100

    [Passive view] Remove unused method

--- src/main/java/usantatecla/draughts/controllers/Controller.java
+++ src/main/java/usantatecla/draughts/controllers/Controller.java
@@ -1,7 +1,5 @@
 package usantatecla.draughts.controllers;
 
-import usantatecla.draughts.models.Color;
-import usantatecla.draughts.models.Coordinate;
 import usantatecla.draughts.models.Game;
 import usantatecla.draughts.models.State;
 
@@ -15,11 +13,6 @@ class Controller {
         assert state != null;
         this.game = game;
         this.state = state;
-    }
-
-    public Color getColor(Coordinate coordinate) {
-        assert coordinate != null;
-        return this.game.getColor(coordinate);
     }
 
     public int getDimension() {
