4b84eaadf0f731f7d19a8330e33906b270b5a4bc
Juan Avila <dev@juanavila.es>
2020-11-22 11:59:49 +0100

    [Passive view] Break cycle

--- src/main/java/usantatecla/draughts/controllers/StartController.java
+++ src/main/java/usantatecla/draughts/controllers/StartController.java
@@ -1,6 +1,8 @@
 package usantatecla.draughts.controllers;
 
+import usantatecla.draughts.models.Coordinate;
 import usantatecla.draughts.models.Game;
+import usantatecla.draughts.models.Piece;
 import usantatecla.draughts.models.State;
 import usantatecla.draughts.views.View;
 
@@ -32,7 +34,19 @@ public class StartController extends InteractorController {
 	void write() {
 		this.view.writeNumbersLine(this.getDimension());
 		for (int i = 0; i < this.getDimension(); i++)
-			this.view.writePiecesRow(i, this);
+			this.writePiecesRow(i);
 		this.view.writeNumbersLine(this.getDimension());
+	}
+
+	void writePiecesRow(final int row) {
+		this.view.write((row + 1) + "");
+		for (int j = 0; j < this.getDimension(); j++) {
+			Piece piece = this.getPiece(new Coordinate(row, j));
+			if (piece == null)
+				this.view.write(" ");
+			else
+				this.view.write(piece.getCode());
+		}
+		this.view.writeln((row + 1) + "");
 	}
 }
